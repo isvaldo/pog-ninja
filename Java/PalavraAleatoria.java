3d78353b30197df78032b9e08e9567af03b8a64f
Josinaldo Albuquerque Barbosa <josinaldo.barbosa@al.infnet.edu.br>
2015-04-14 18:52:33 -0300

    Create PalavraAleatoria.java

---
+++ Java/PalavraAleatoria.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Nosso alfabeto
+		String caracteres = "abcdefghijklmnopqrstuvwxyz";
+
+		// Instancia classe Random
+		Random random = new Random();
+
+		// Tamanho aleatório entre 1 e 10
+		int tamanhoPalavra = random.nextInt(10) + 1;
+
+		// Onde ira armazenar a palavra
+		char[] palavra = new char[tamanhoPalavra];
+
+		// Incrementa caracter a caracter aleatóriamente
+		for (int i = 0; i < tamanhoPalavra; i++) {
+
+			// Gera uma caracter aleatório de 0 a tamanho da quantidade de caracteres - 1
+			int caracterRandom = random.nextInt(caracteres.length());
+
+			// Palavra na posição de i receber um caracter na posição aleatória
+			palavra[i] = caracteres.charAt(caracterRandom);
+		}
+
+		// Exibe a palavra aleatória
+		System.out.println(palavra);
+
+	}
+
+}
